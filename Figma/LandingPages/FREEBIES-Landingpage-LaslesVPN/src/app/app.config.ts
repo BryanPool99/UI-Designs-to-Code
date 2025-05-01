8ff22401d68fc885508df02f5c11e93c7442a4af
BryanPool99 <bryanpool991@hotmail.com>
2025-05-01 16:47:38 -0500

    chore: init structure project Angular with i18n

---
+++ Figma/LandingPages/FREEBIES-Landingpage-LaslesVPN/src/app/app.config.ts
@@ -0,0 +1,16 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+import { i18nConfig } from './core/config/i18n.config';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
+    provideClientHydration(),
+    i18nConfig.providers,
+  ],
+};
